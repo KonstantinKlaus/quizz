babc35b467e9063dcd3a9a0ec9d2aca83c0a49cf
Konstantin Klaus <konstantin.klaus@uni-ulm.de>
2019-12-28 21:37:03 +0100

    Initial commit

---
+++ question_loader/question_loader.py
@@ -0,0 +1,18 @@
+import urllib.request, json
+
+base_url = "https://opentdb.com/api.php?"
+
+def getQuestions(difficulty="",category="", number=10):
+    amount = "amount=" + str(number)
+    generated_url = base_url + amount
+    if category != "":
+        generated_url= generated_url + "&" + "category=" + category
+
+    if difficulty != "":
+        generated_url= generated_url + "&" + "difficulty=" + difficulty
+
+    with urllib.request.urlopen(generated_url) as url:
+        data = json.loads(url.read().decode())
+        print(data)
+
+    return data
