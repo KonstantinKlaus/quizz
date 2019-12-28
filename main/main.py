4b1545f38ea9989a17fefe27ba9f57232d216b65
Konstantin Klaus <konstantin.klaus@uni-ulm.de>
2019-12-28 22:26:55 +0100

    logging

--- main/main.py
+++ main/main.py
@@ -1,1 +1,12 @@
+from question_loader import question_loader
+import logging
 
+# logger
+logger = logging.getLogger("log")
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+logger.addHandler(ch)
+
+
+question_loader.getQuestions()
