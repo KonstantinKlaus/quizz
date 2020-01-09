01c90528644540dd960b831c064963b264c4c0d4
Konstantin Klaus <konstantin.klaus@uni-ulm.de>
2020-01-09 19:42:41 +0100

    added game modes

---
+++ gamemodes/classic.py
@@ -0,0 +1,61 @@
+from gamemodes.game_mode import GameMode
+
+__author__ = "Konstantin Klaus"
+
+class ClassicGame(GameMode):
+
+    def load_questions(self):
+        self.questions = self.question_db.get_questions(10)
+
+    def run_game(self):
+        self.game_running = True
+        while self.game_running:
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+            self.clock.tick(30)
+
+    def on_loop(self):
+        pass
+
+    def on_render(self):
+        self.screen.fill(WHITE)
+
+        (width, height) = self.screen.get_size()
+
+        # Menu Buttons
+        rec1 = pygame.Rect(0.1 * width, 0.2 * height, 0.8 * width, 0.15 * height)
+        rec2 = pygame.Rect(0.1 * width, 0.4 * height, 0.8 * width, 0.15 * height)
+        rec3 = pygame.Rect(0.1 * width, 0.6 * height, 0.8 * width, 0.15 * height)
+        rec4 = pygame.Rect(0.1 * width, 0.8 * height, 0.8 * width, 0.15 * height)
+
+        pygame.draw.rect(self.screen, BLUE, rec1)
+        pygame.draw.rect(self.screen, ORANGE, rec2)
+        pygame.draw.rect(self.screen, GREEN, rec3)
+        pygame.draw.rect(self.screen, YELLOW, rec4)
+
+        # Button Text
+        font = pygame.font.Font('freesansbold.ttf', int(0.075 * height))
+
+        text0 = font.render(question, True, BLACK)
+        text1 = font.render(answer1, True, BLACK)
+        text2 = font.render(answer2, True, BLACK)
+        text3 = font.render(answer3, True, BLACK)
+        text4 = font.render(answer4, True, BLACK)
+
+        self.screen.blit(text0, (0.5 * width - text1.get_width() // 2, 0.1 * height - text1.get_height() // 2))
+        self.screen.blit(text1, (0.5 * width - text1.get_width() // 2, 0.275 * height - text1.get_height() // 2))
+        self.screen.blit(text2, (0.5 * width - text2.get_width() // 2, 0.475 * height - text2.get_height() // 2))
+        self.screen.blit(text3, (0.5 * width - text3.get_width() // 2, 0.675 * height - text3.get_height() // 2))
+        self.screen.blit(text4, (0.5 * width - text4.get_width() // 2, 0.875 * height - text4.get_height() // 2))
+
+        pygame.display.update()
+
+
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self.game.end_game()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                self.game.end_game()
--- gamemodes/game_mode.py
+++ gamemodes/game_mode.py
@@ -2,4 +2,28 @@ class GameMode:
 
     questions = []
 
-    
+    position = 0
+
+    game_running = False
+
+    def __init__(self, game, question_db):
+        self.game = game
+        self.question_db = question_db
+
+    def get_current_question(self):
+        return self.questions[self.position]
+
+    def next_question(self):
+        if len(self.questions)==self.position:
+            pass
+        else:
+            self.position = self.position + 1
+
+    def get_next_question(self):
+        if not len(self.questions)==self.position:
+            self.position = self.position + 1
+
+        return self.questions[self.position]
+
+    def get_number_questions_left(self):
+        return len(self.questions) - self.position
