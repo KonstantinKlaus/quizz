8816636c79769b583c2e9dc3d9c5c74fc3a0e369
Konstantin Klaus <konstantinmaxklaus@gmail.com>
2020-01-11 18:46:46 +0100

    moved answers

--- gamemodes/classic.py
+++ gamemodes/classic.py
@@ -71,20 +71,20 @@ class ClassicGame(GameMode):
             index = self.current_question().index_correct_answer()
             text = font.render(answers[index], True, BLACK)
 
-            rec = pygame.Rect(0.1 * width, (0.2 + index * 0.2) * height, 0.8 * width, 0.15 * height)
+            rec = pygame.Rect(0.1 * width, (0.25 + index * 0.2) * height, 0.8 * width, 0.15 * height)
 
             pygame.draw.rect(self.screen, get_color_by_index(index), rec)
 
             self.screen.blit(text, (0.5 * width - text.get_width() // 2,
-                                    (0.275 + index * 0.2) * height - text.get_height() // 2))
+                                    (0.325 + index * 0.2) * height - text.get_height() // 2))
 
         else:
 
             # Answer Buttons
-            rec1 = pygame.Rect(0.1 * width, 0.2 * height, 0.8 * width, 0.15 * height)
-            rec2 = pygame.Rect(0.1 * width, 0.4 * height, 0.8 * width, 0.15 * height)
-            rec3 = pygame.Rect(0.1 * width, 0.6 * height, 0.8 * width, 0.15 * height)
-            rec4 = pygame.Rect(0.1 * width, 0.8 * height, 0.8 * width, 0.15 * height)
+            rec1 = pygame.Rect(0.1 * width, 0.25 * height, 0.8 * width, 0.15 * height)
+            rec2 = pygame.Rect(0.1 * width, 0.45 * height, 0.8 * width, 0.15 * height)
+            rec3 = pygame.Rect(0.1 * width, 0.65 * height, 0.8 * width, 0.15 * height)
+            rec4 = pygame.Rect(0.1 * width, 0.85 * height, 0.8 * width, 0.15 * height)
 
             pygame.draw.rect(self.screen, BLUE, rec1)
             pygame.draw.rect(self.screen, ORANGE, rec2)
@@ -96,10 +96,10 @@ class ClassicGame(GameMode):
             text3 = font.render(answers[2], True, BLACK)
             text4 = font.render(answers[3], True, BLACK)
 
-            self.screen.blit(text1, (0.5 * width - text1.get_width() // 2, 0.275 * height - text1.get_height() // 2))
-            self.screen.blit(text2, (0.5 * width - text2.get_width() // 2, 0.475 * height - text2.get_height() // 2))
-            self.screen.blit(text3, (0.5 * width - text3.get_width() // 2, 0.675 * height - text3.get_height() // 2))
-            self.screen.blit(text4, (0.5 * width - text4.get_width() // 2, 0.875 * height - text4.get_height() // 2))
+            self.screen.blit(text1, (0.5 * width - text1.get_width() // 2, 0.325 * height - text1.get_height() // 2))
+            self.screen.blit(text2, (0.5 * width - text2.get_width() // 2, 0.525 * height - text2.get_height() // 2))
+            self.screen.blit(text3, (0.5 * width - text3.get_width() // 2, 0.725 * height - text3.get_height() // 2))
+            self.screen.blit(text4, (0.5 * width - text4.get_width() // 2, 0.925 * height - text4.get_height() // 2))
 
         pygame.display.update()
 
